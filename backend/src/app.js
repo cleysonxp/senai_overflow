452b519731b18d1cc8b5a38411be9304875e85db
cleyson.ribeiro.777@gmail.com <cleyson.ribeiro.777@gmail.com>
2020-08-18 11:27:12 -0300

    Finalização do backend com autenticação JWT

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,14 @@
 const express = require('express');
+const cors = require("cors")
 require("./database");
 const rotas = require("./routes");
 
 
 //iniciando a aplicação
 const app = express()
+
+//habilitando o cors para qualquer origem
+app.use(cors())
 
 //nas requisições pode ter corpo
 app.use(express.json());
--- backend/src/serve.js
+++ backend/src/serve.js
@@ -1,5 +1,5 @@
 const app = require("./app");
 
-app.listen(3000, () =>{
-    console.log("Servidor rodando na porta 3000");
+app.listen(3333, () =>{
+    console.log("Servidor rodando na porta 3333");
 });
